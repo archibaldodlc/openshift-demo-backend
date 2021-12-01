fa2fd39c467d70220de7cf8e7735757dd2db25ac
Archibaldo De La Cruz <victor.merino@archibaldo.cl>
2021-12-01 04:15:41 -0300

    Install all rpm at once

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,12 +6,7 @@ curl -v -O https://repo.mongodb.org/yum/redhat/6/mongodb-org/4.4/x86_64/RPMS/mon
 curl -v -O https://repo.mongodb.org/yum/redhat/6/mongodb-org/4.4/x86_64/RPMS/mongodb-org-mongos-4.4.9-1.el6.x86_64.rpm -L
 curl -v -O https://repo.mongodb.org/yum/redhat/6/mongodb-org/4.4/x86_64/RPMS/mongodb-org-shell-4.4.9-1.el6.x86_64.rpm -L
 curl -v -O https://repo.mongodb.org/yum/redhat/6/mongodb-org/4.4/x86_64/RPMS/mongodb-org-4.4.9-1.el6.x86_64.rpm -L 
-yum install -y mongodb-org-server-4.4.9-1.el6.x86_64.rpm
-yum install -y mongodb-org-shell-4.4.9-1.el6.x86_64.rpm
-yum install -y mongodb-org-mongos-4.4.9-1.el6.x86_64.rpm
-yum install -y mongodb-org-tools-4.4.9-1.el6.x86_64.rpm
-yum install -y mongodb-org-4.4.9-1.el6.x86_64.rpm
-yum install -y mongodb-org-database-tools-extra-4.4.9-1.el6.x86_64.rpm
+rpm -ivh *.rpm
 systemctl daemon-reload
 systemctl start mongod
 systemctl enable mongod
