23d315d1ee6ca1a9687ac8622efeba419120583f
Archibaldo De La Cruz <victor.merino@archibaldo.cl>
2021-12-01 03:20:21 -0300

    [Test] alternative

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 sudo yum install java-1.8.0-openjdk -y
-sudo cp scripts/mongodb.repo /etc/yum.repos.d/
+sudo cat >/etc/yum.repos.d/mongodb.repo <<EOL
+[mongodb-org-5.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/5.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc
+EOL
 sudo yum update -y
 sudo yum -y mongodb-org
 sudo systemctl daemon-reload
