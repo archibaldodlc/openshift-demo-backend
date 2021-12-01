93d41e65e166bf9466fa16a0c3e07f40a7222e76
Archibaldo De La Cruz <victor.merino@archibaldo.cl>
2021-12-01 20:27:38 -0300

    Adding script to install mongodb and configure it

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 yum install java-1.8.0-openjdk -y
-curl -v -O https://repo.mongodb.org/yum/redhat/6/mongodb-org/4.4/x86_64/RPMS/mongodb-org-database-tools-extra-4.4.9-1.el6.x86_64.rpm -L
-curl -v -O https://repo.mongodb.org/yum/redhat/6/mongodb-org/4.4/x86_64/RPMS/mongodb-org-tools-4.4.9-1.el6.x86_64.rpm -L
-curl -v -O https://repo.mongodb.org/yum/redhat/6/mongodb-org/4.4/x86_64/RPMS/mongodb-org-server-4.4.9-1.el6.x86_64.rpm -L
-curl -v -O https://repo.mongodb.org/yum/redhat/6/mongodb-org/4.4/x86_64/RPMS/mongodb-org-mongos-4.4.9-1.el6.x86_64.rpm -L
-curl -v -O https://repo.mongodb.org/yum/redhat/6/mongodb-org/4.4/x86_64/RPMS/mongodb-org-shell-4.4.9-1.el6.x86_64.rpm -L
-curl -v -O https://repo.mongodb.org/yum/redhat/6/mongodb-org/4.4/x86_64/RPMS/mongodb-org-4.4.9-1.el6.x86_64.rpm -L 
-rpm -ivh *.rpm
+cat >/etc/yum.repos.d/mongodb-org-5.0.repo <<EOL
+[mongodb-org-5.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/7/mongodb-org/5.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc
+EOL
+yum install mongodb-org -y
 systemctl daemon-reload
 systemctl start mongod
 systemctl enable mongod
+echo 'db.createUser({ user: "mongodbtest", pwd: "password123", roles: [{ role: "readWrite", db: "test" }]});' | mongo test
