c4480fdc665d4b095dc634d08713d174098f9d61
Archibaldo De La Cruz <victor.merino@archibaldo.cl>
2021-12-01 03:28:01 -0300

    Run script as root

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum install java-1.8.0-openjdk -y
-sudo cat >/etc/yum.repos.d/mongodb.repo <<EOL
+yum install java-1.8.0-openjdk -y
+cat >/etc/yum.repos.d/mongodb.repo <<EOL
 [mongodb-org-5.0]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/5.0/x86_64/
@@ -8,8 +8,8 @@ gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc
 EOL
-sudo yum update -y
-sudo yum -y mongodb-org
-sudo systemctl daemon-reload
-sudo systemctl start mongod
-sudo systemctl enable mongod
+yum update -y
+yum -y mongodb-org
+systemctl daemon-reload
+systemctl start mongod
+systemctl enable mongod
