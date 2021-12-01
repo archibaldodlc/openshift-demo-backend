d47438ff2f26773699309ab1ff578ac2bc205685
Archibaldo De La Cruz <victor.merino@archibaldo.cl>
2021-12-01 03:56:04 -0300

    Added mongo shell

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 yum install java-1.8.0-openjdk -y
+curl -v -O https://repo.mongodb.org/yum/redhat/6/mongodb-org/4.4/x86_64/RPMS/mongodb-org-shell-4.4.9-1.el6.x86_64.rpm -L
+yum install -y mongodb-org-shell-4.0.9-1.el7.x86_64.rpm
 curl -v -O https://repo.mongodb.org/yum/redhat/6/mongodb-org/4.4/x86_64/RPMS/mongodb-org-4.4.9-1.el6.x86_64.rpm -L 
 yum install -y mongodb-org-4.4.9-1.el6.x86_64.rpm
 yum update -y
