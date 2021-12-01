2ae1cf80f991dcca65a44e2a0d0f145d5c4e04f1
Archibaldo De La Cruz <victor.merino@archibaldo.cl>
2021-12-01 03:14:54 -0300

    Fix path

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum install java-1.8.0-openjdk -y
-sudo cp ./mongodb.repo /etc/yum.repos.d/
+sudo cp scripts/mongodb.repo /etc/yum.repos.d/
 sudo yum update -y
 sudo yum -y mongodb-org
 sudo systemctl daemon-reload
